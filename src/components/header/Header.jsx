27507d05af792b5988f305c33b90eba33d03ebfd
Sakib12345 <sakibalislam12345@gmail.com>
2022-05-15 14:24:19 +0600

    issue solved of design

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,37 @@
+import {faBed, faCar, faMagnet, faPlane, faTaxi} from '@fortawesome/free-solid-svg-icons'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import React from 'react'
+import './header.css'
+
+const Header = () => {
+  return (
+    <div className="header">
+        <div className="headerContainer">
+            <div className="headerList">
+                <div className="headerListItem">
+                    <FontAwesomeIcon icon={faBed} />
+                    <span>Stays</span>
+                </div>
+                <div className="headerListItem">
+                    <FontAwesomeIcon icon={faPlane} />
+                    <span>Flights</span>
+                </div>
+                <div className="headerListItem">
+                    <FontAwesomeIcon icon={faCar} />
+                    <span>Car Rentals</span>
+                </div>
+                <div className="headerListItem">
+                    <FontAwesomeIcon icon={faMagnet} />
+                    <span>Attractions</span>
+                </div>
+                <div className="headerListItem">
+                    <FontAwesomeIcon icon={faTaxi} />
+                    <span>Airport Taxi</span>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Header
