9b16b29f87639753ab938270510788e057b51341
sakib12345 <sakib35-1680@diu.edu.bd>
2022-05-16 13:53:48 +0600

    hover issue fixed

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,44 +1,58 @@
-import {faBed, faCar, faMagnet, faPlane, faTaxi} from '@fortawesome/free-solid-svg-icons'
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import { faBed, faCalendarDays, faCar, faMagnet, faPerson, faPlane, faTaxi } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
 import './header.css'
 
 const Header = () => {
-  return (
-    <div className="header">
-        <div className="headerContainer">
-            <div className="headerList">
-                <div className="headerListItem active">
-                    <FontAwesomeIcon icon={faBed} />
-                    <span>Stays</span>
+    return (
+        <div className="header">
+            <div className="headerContainer">
+                <div className="headerList">
+                    <div className="headerListItem active">
+                        <FontAwesomeIcon icon={faBed} />
+                        <span>Stays</span>
+                    </div>
+                    <div className="headerListItem">
+                        <FontAwesomeIcon icon={faPlane} />
+                        <span>Flights</span>
+                    </div>
+                    <div className="headerListItem">
+                        <FontAwesomeIcon icon={faCar} />
+                        <span>Car Rentals</span>
+                    </div>
+                    <div className="headerListItem">
+                        <FontAwesomeIcon icon={faMagnet} />
+                        <span>Attractions</span>
+                    </div>
+                    <div className="headerListItem">
+                        <FontAwesomeIcon icon={faTaxi} />
+                        <span>Airport Taxi</span>
+                    </div>
                 </div>
-                <div className="headerListItem">
-                    <FontAwesomeIcon icon={faPlane} />
-                    <span>Flights</span>
-                </div>
-                <div className="headerListItem">
-                    <FontAwesomeIcon icon={faCar} />
-                    <span>Car Rentals</span>
-                </div>
-                <div className="headerListItem">
-                    <FontAwesomeIcon icon={faMagnet} />
-                    <span>Attractions</span>
-                </div>
-                <div className="headerListItem">
-                    <FontAwesomeIcon icon={faTaxi} />
-                    <span>Airport Taxi</span>
+                <h1 className="headerTitle">A Lifetime of Discounts? It's Genius.</h1>
+                <p className="headerDescription">
+                    Get Rewarded for your travels - unlock instant savings of 10% or more with a free Hotels.com account.
+                </p>
+                <button className="headerBtn">
+                    Sign In / Register
+                </button>
+                <div className="headerSearch">
+                    <div className="headerSearchItem">
+                        <FontAwesomeIcon icon={faBed} className="headerIcon" />
+                        <input type="text" name="" className="headerSearchInput" id="" placeholder="Where are you going?" />
+                    </div>
+                    <div className="headerSearchItem">
+                        <FontAwesomeIcon icon={faCalendarDays} className="headerIcon" />
+                        <span className="headerSearchText">date to date</span>
+                    </div>
+                    <div className="headerSearchItem">
+                        <FontAwesomeIcon icon={faPerson} className="headerIcon" />
+                        <span className="headerSearchText">2 adults 2 children 1 room</span>
+                    </div>
                 </div>
             </div>
-            <h1 className="headerTitle">A Lifetime of Discounts? It's Genius.</h1>
-            <p className="headerDescription">
-                Get Rewarded for your travels - unlock instant savings of 10% or more with a free Hotels.com account.
-            </p>
-            <button className="headerBtn">
-                Sign In / Register
-            </button>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Header
